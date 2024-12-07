f8bae8ba94a61b60989d5e797bcdc8f263854037
polaris <61411085+polaris428@users.noreply.github.com>
2024-12-08 02:06:39 +0900

    Initial commit

---
+++ FallingEffectCustomLibrary/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.polaris.fallingeffectcustomlibrary"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            create("release", MavenPublication::class.java) {
+                // Applies the component for the release build variant.
+                from(components["release"])
+
+                // You can then customize attributes of the publication as shown below.
+                groupId = "com.github.polaris428"
+                artifactId = "FallingEffectCustomLibrary"
+                version = "0.0.1"
+            }
+
+            // Creates a Maven publication called "debug".
+            create("debug", MavenPublication::class.java) {
+                // Applies the component for the debug build variant.
+                from(components["debug"])
+
+                groupId = "com.github.polaris428"
+                artifactId = "FallingEffectCustomLibrary"
+                version = "0.0.1"
+            }
+        }
+    }
+}
+
+group="com.github.polaris428"
